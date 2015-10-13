f131a70c567ec3b1775192ebd35dfeaae6cd0245
Chris Beams <chris@gradle.com>
2015-10-13 11:57:12 +0200

    Introduce org.astro module dependency
    This step adds a second module named `org.astro` and updates the
    `com.greetings` module declaration to _require_ a package _exported_ by
    the `org.astro` module (the exported package is also named `org.astro`).
    
    The modules are compiled separately. In order for `com.greetings` to
    have access to the `org.astro` module's contents, the `javac` command is
    given a `-modulepath` argument where the `org.astro` module and the
    types in its exported packages can be resolved.
    
    The main method in `com.greeting` has been updated as well, printing out
    the name of a type referenced from the `org.astro` module. The output to
    the console should now read:
    
        $ ./step.sh
        ...
        Greetings from org.astro's world!
    
    http://openjdk.java.net/projects/jigsaw/quick-start#greetingsworld

--- src/com.greetings/com/greetings/Main.java
+++ src/com.greetings/com/greetings/Main.java
@@ -3,7 +3,7 @@ package com.greetings;
 public class Main {
 
     public static void main(String[] args) {
-        Object name = "main";
+        Object name = org.astro.World.name();
         System.out.format("Greetings from %s!\n", name);
     }
 }
